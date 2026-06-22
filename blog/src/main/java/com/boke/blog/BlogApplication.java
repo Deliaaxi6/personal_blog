8b7d35003681b7bf448658bc3f841860531578f7
Deliaaxi6 <162301338+Deliaaxi6@users.noreply.github.com>
2026-06-22 20:04:01 +0800

    初始化搭建，待完善

--- blog/src/main/java/com/boke/blog/BlogApplication.java
+++ blog/src/main/java/com/boke/blog/BlogApplication.java
@@ -1,13 +1,14 @@
 package com.boke.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.boke.blog.mapper")
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
 }
